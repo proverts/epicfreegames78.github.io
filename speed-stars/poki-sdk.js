a934c579f260ab001a9f16c623d3544b58a477f7
proverts <ggage4398@gmail.com>
2026-09-09 18:29:27 -0500

    Add PokiSDK with basic game lifecycle methods

--- speed-stars/poki-sdk.js
+++ speed-stars/poki-sdk.js
@@ -1,1 +1,8 @@
-
+window.PokiSDK = {
+init: function() { return Promise.resolve(true); },
+gameLoadingFinished: function() {},
+gameplayStart: function() {},
+gameplayStop: function() {},
+commercialBreak: function() { return Promise.resolve(true); },
+rewardedBreak: function() { return Promise.resolve(true); }
+};
