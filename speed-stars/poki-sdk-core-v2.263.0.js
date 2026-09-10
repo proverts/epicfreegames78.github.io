4c96685d91f8ecfe4faaf1fe6d3ec9e907f971f4
proverts <ggage4398@gmail.com>
2026-09-09 19:54:33 -0500

    Refactor PokiSDK methods for improved functionality

--- speed-stars/poki-sdk-core-v2.263.0.js
+++ speed-stars/poki-sdk-core-v2.263.0.js
@@ -1,8 +1,31 @@
 window.PokiSDK = {
-init: function() { return Promise.resolve(true); },
+init: function() {
+return Promise.resolve(true);
+},
+initWithPoki: function() {
+return Promise.resolve(true);
+},
+gameLoadingStart: function() {},
 gameLoadingFinished: function() {},
 gameplayStart: function() {},
 gameplayStop: function() {},
-commercialBreak: function() { return Promise.resolve(true); },
-rewardedBreak: function() { return Promise.resolve(true); }
+commercialBreak: function(fn) {
+if (typeof fn === 'function') fn();
+return Promise.resolve(true);
+},
+rewardedBreak: function(fn) {
+if (typeof fn === 'function') fn(true);
+return Promise.resolve(true);
+},
+displayAd: function(container, size, fn) {
+if (typeof fn === 'function') fn();
+return Promise.resolve(true);
+},
+destroyAd: function() {},
+setDebug: function() {},
+getParam: function() { return ""; },
+getURLParam: function() { return ""; },
+captureError: function() {},
+measure: function() {},
+isAdBlocked: function() { return false; }
 };
